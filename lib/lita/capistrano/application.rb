c22a43bde8e7c77e94cce943609cdecef55af267
Vlad Verestiuc <verestiuc.vlad@gmail.com>
2016-06-03 08:10:16 +0300

    added more commands

--- lib/lita/capistrano/application.rb
+++ lib/lita/capistrano/application.rb
@@ -3,21 +3,18 @@ module Lita # :nodoc:
   module Capistrano # :nodoc:
     class Application
 
-      attr_reader :name, :environment, :command
-      def initialize(name:, command:, environment:)
+      attr_reader :name, :environment, :ssh
+      def initialize(name:, environment:, ssh: )
         @name = name
-        @command = command
+        @ssh = ssh
         @environment = environment
       end
 
-      def deploy(ssh:, bot:)
-        bot.reply "Deploying application #{name} in #{environment}"
-        Net::SSH.start(ssh[:server], ssh[:ssh_options].fetch(:user), ssh[:ssh_options]) do |connection|
+      def deploy(bot)
+        bot.reply "Deploying application #{name} on #{environment}"
+        Net::SSH.start(ssh[:server], ssh[:user], ssh[:ssh_options]) do |connection|
           connection.open_channel do |ch|
-            hash = match_data_to_hash(bot.match_data)
-            cmd = command % hash
-            Lita.logger.debug("Executing #{cmd} on #{ssh[:server]}")
-            ch.exec cmd do |_ch, success|
+            ch.exec "#{name} #{environment}" do |_ch, success|
               return bot.reply('could not execute command') unless success
               ch.on_data do |_c, data|
                 Lita.logger.debug(data)
@@ -30,29 +27,7 @@ module Lita # :nodoc:
             end
           end.wait
         end
-        bot.reply "Done deploying #{name} in #{environment}"
-      end
-
-      class <<self
-
-        def find_by(name:, environment:)
-          yaml = Lita.redis["capistrano:#{environment}:#{name}"]
-          yaml && YAML.load(yaml)
-        end
-
-        def create(name:, command:, environment:)
-          instance = new(name: name, command: command, environment: environment)
-          Lita.redis["capistrano:#{environment}:#{name}"] = instance.to_yaml
-        end
-
-      end
-
-      private
-
-      def match_data_to_hash(match_data)
-        Hash[match_data.names.zip(match_data.captures)].each_with_object({}) do |(k, v), memo|
-          memo[k.to_sym] = v
-        end
+        bot.reply "Finished deploying #{name} on #{environment}"
       end
 
     end
--- lib/lita/handlers/capistrano.rb
+++ lib/lita/handlers/capistrano.rb
@@ -4,44 +4,94 @@ module Lita
 
       config :ssh, type: Hash, required: true
 
-      route(/^(?<application>[^\s]+) is deployed in (?<environment>[^\s]+) using ```(?<command>.+)```$/,
-            :add_application, command: :true, help:
-            { 'APP is deployed in ENVIRONMENT using "SHELL" command' => 'example.com is deployed in staging using ```bundle exec cap staging deploy```' })
-
       route(/^deploy (?<application>.+) (on|in) (?<environment>.*)$/,
             :deploy_application, command: :true, help:
-            { 'deploy NAME on ENVIRONMENT' => 'deploy example.com in staging' })
+            { 'deploy NAME on ENVIRONMENT' => 'deploy example.com on staging' })
+
+      route(/^(?<action>allow|disallow) (?<handle>.+) to deploy (?<application>.+) (on|in) (?<environment>.*)$/,
+            :manage_deployment, command: :true, help:
+            { '(dis)allow HANDLE to deploy APP on ENVIRONMENT' => 'allow @chuck to deploy example.com on staging' })
+
+      route(/^who can deploy (?<application>.+)( (on|in) (?<environment>.+))?\s?\?$/,
+            :show_members, command: :true, help:
+            { 'who can deploy APP( on ENVIRONMENT)?' => 'show who is alloed to deploy' })
+
+      route(/^what can be deployed\s?\?$/,
+            :show_applications, command: :true, help:
+            { 'what can be deployed?' => 'shows the applications that can be deployed' })
 
       def deploy_application(response)
-        name = response.match_data[:application]
-        environment = response.match_data[:environment]
-        application = find_application(name: name, environment: environment)
-        return response.reply "#{name} not found" unless application
-        application.deploy(bot: response, ssh: config.ssh)
+        app = Lita::Capistrano::Application.new(
+          name: response.match_data[:application],
+          environment: response.match_data[:environment],
+          ssh: config.ssh
+        )
+
+        if deploy_allowed?(app.name, app.environment, response.user)
+          app.deploy(response)
+        else
+          response.reply("You're not allowed to deploy #{app.name} on #{app.environment}")
+        end
       rescue => e
         exception_handler(e, response)
         raise
       end
 
-      def add_application(response)
-        name = response.match_data[:application]
-        command = response.match_data[:command]
+      def manage_deployment(response)
+        return unless Lita.config.robot.admins.include?(response.user.id)
+        text = response.match_data[:handle]
+        text = text[1..-1] if text.start_with?("@")
+        user = Lita::User.fuzzy_find(text)
         environment = response.match_data[:environment]
-        application = find_application(name: name, environment: environment)
-        return response.reply "application #{name} already exists" if application
+        app = response.match_data[:application]
+        return response.reply("Unable to find a matching user for #{response.match_data[:handle]}") unless user
+        Lita.logger.debug user.inspect
+        if response.match_data[:action] == "allow"
+          Lita.redis.sadd("capistrano:#{app}:#{environment}:members", user.id)
+          response.reply("#{user.name} can deploy #{app} on #{environment}")
+        else
+          Lita.redis.srem("capistrano:#{app}:#{environment}:members", user.id)
+          response.reply("#{user.name} can't deploy #{app} on #{environment} anymore")
+        end
+      end
 
-        Lita::Capistrano::Application.create(
-          name: name,
-          command: command,
-          environment: environment
-        )
-        response.reply "Ok now I known how to deploy #{name} in #{environment}"
-      rescue => e
-        exception_handler(e, response)
-        raise
+      def show_members(response)
+        environments = [response.match_data[:environment]].compact
+        app = response.match_data[:application]
+        Lita.logger.debug environments.inspect
+        environments = Lita.redis.keys("capistrano:#{app}:*:members").map do |key|
+          key.split(":")[-2]
+        end.uniq if environments.empty?
+
+        environments.each do |environment|
+          names = Lita.redis.smembers("capistrano:#{app}:#{environment}:members").map do |id|
+            Lita::User.find_by_id(id)
+          end.map(&:mention_name).join(", ")
+
+          response.reply "#{app} can be deployed by #{names} on #{environment}"
+        end
+      end
+
+      def show_applications(response)
+        apps = Lita.redis.keys("capistrano:*:*:members").each_with_object({}) do |key, app|
+          parts = key.split(":")
+          (app[parts[-3]] ||= Set.new) << parts[-2]
+        end
+
+        if apps.empty?
+          response.reply "There is nothing that can be deployed"
+        else
+          apps.each do |app, environments|
+            response.reply "#{app} can be deployed on #{environments.to_a.join(", ")}"
+          end
+        end
       end
 
       private
+
+      def deploy_allowed?(app, environment, user)
+        Lita.redis.sismember("capistrano:#{app}:#{environment}:members", user.id)
+      end
 
       def exception_handler(exception, response)
         relevant_lines = filter_backtrace(exception.backtrace)
@@ -52,10 +102,6 @@ module Lita
         lines.select do |line|
           line.start_with? File.expand_path("../../../../", __FILE__)
         end
-      end
-
-      def find_application(name:, environment:)
-        Lita::Capistrano::Application.find_by(name: name, environment: environment)
       end
 
       Lita.register_handler(self)
