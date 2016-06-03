1dc56506a7046914910e71ca360c904aa017f479
Vlad Verestiuc <verestiuc.vlad@gmail.com>
2016-06-03 03:56:19 +0300

    Change add application syntax due to utf-8 errors

--- lib/lita/handlers/capistrano.rb
+++ lib/lita/handlers/capistrano.rb
@@ -4,9 +4,9 @@ module Lita
 
       config :ssh, type: Hash, required: true
 
-      route(/^(?<application>.+) is deployed in (?<environment>[^\s]+) using "(?<command>.*)("|”) command$/,
+      route(/^(?<application>[^\s]+) is deployed in (?<environment>[^\s]+) using ```(?<command>.+)```$/,
             :add_application, command: :true, help:
-            { 'APP is deployed in ENVIRONMENT using "SHELL" command' => 'example.com is deployed in staging using "bundle exec cap staging deploy" command' })
+            { 'APP is deployed in ENVIRONMENT using "SHELL" command' => 'example.com is deployed in staging using ```bundle exec cap staging deploy```' })
 
       route(/^deploy (?<application>.+) (on|in) (?<environment>.*)$/,
             :deploy_application, command: :true, help:
@@ -18,6 +18,9 @@ module Lita
         application = find_application(name: name, environment: environment)
         return response.reply "#{name} not found" unless application
         application.deploy(bot: response, ssh: config.ssh)
+      rescue => e
+        exception_handler(e, response)
+        raise
       end
 
       def add_application(response)
@@ -33,9 +36,23 @@ module Lita
           environment: environment
         )
         response.reply "Ok now I known how to deploy #{name} in #{environment}"
+      rescue => e
+        exception_handler(e, response)
+        raise
       end
 
       private
+
+      def exception_handler(exception, response)
+        relevant_lines = filter_backtrace(exception.backtrace)
+        response.reply "I've encounterd #{exception.message} error at ```#{relevant_lines.join("\n")}```"
+      end
+
+      def filter_backtrace(lines)
+        lines.select do |line|
+          line.start_with? File.expand_path("../../../../", __FILE__)
+        end
+      end
 
       def find_application(name:, environment:)
         Lita::Capistrano::Application.find_by(name: name, environment: environment)
